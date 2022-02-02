17402e07f1777392e49f5ea5f02dc2bfabba8e4e
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-02-02 23:58:10 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -4,16 +4,34 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 opts = Options()
 opts.add_argument(" --headless")
 opts.binary_location= os.getcwd() +'"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"'
 chrome_driver = os.getcwd() +"C:\\Users\\HP\\Downloads\\chromedriver_win32"
-
-what_to_search = input('Search what? Huh?')
-url='https://shopee.ph/search?keyword=' + what_to_search + '&shop=169850668'
+shops = {'ab':'169850668','mo':'190924746', 'fo':'266213560', 'kj':'204666159'}
+# abukay yung may singles
+# moldandmore yung may singles pero nasa kada
+# fortress vmall yung may mamahaling singles
+# kjhobbies yung mura af
+what_to_search = input('Search what? Huh? ')
+shop = input('In what shop? Huh? (ab, mo, fo, kj) ')
+url='https://shopee.ph/search?keyword=' + what_to_search + '&shop=' + shops[shop]
 s=Service('C:/Users/HP/Downloads/chromedriver_win32/chromedriver.exe')
 browser = webdriver.Chrome(service=s)
 browser.get(url)
+try:
+	height = browser.execute_script("return document.documentElement.scrollHeight")
+	for i in range(0,height,50):
+		q = "window.scrollTo(0," + str(i) + ");"
+		browser.execute_script(q) #document.body.scrollHeight
+	myElem = WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'shopee-page-controller')))
+	print("Done")
+except TimeoutException:
+	print("Fuck")
 soup_file = browser.page_source
 soup = BeautifulSoup(soup_file, "html.parser")
 results = soup.find(id="main")
