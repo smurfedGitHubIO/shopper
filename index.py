00abbfbd2f1419cceaf81d564a63c834e6678e21
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-02-02 21:30:04 +0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = ""
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"}
+PRICE = 69420
+
+def getPrice():
+    pass
